09195cab8b948057aed03fb803e91e58b0d8a93c
julig <juliethgs0101@gmail.com>
2024-05-28 21:30:41 -0500

    Se creo una API con spring boot

---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,43 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.model.DatosEpisodio;
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Arrow&apikey=716b5b12");
+		System.out.println(json);
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
+		json= consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Arrow&Season=1&episode=1&apikey=716b5b12");
+		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 0; i < datos.totalDeTemporadas(); i++) {
+			json= consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Arrow&Season="+i+"&apikey=716b5b12");
+			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporadas);
+		}
+
+		temporadas.forEach(System.out::println);
+	}
+}
