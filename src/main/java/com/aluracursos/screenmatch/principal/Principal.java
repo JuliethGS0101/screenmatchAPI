c10765c2538f959cc704119f4ae4b9de9932800b
julig <juliethgs0101@gmail.com>
2024-05-29 22:19:28 -0500

    actualizacion

---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,17 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+
+    public void muestraElMenu(){
+        System.out.println("Por favor escribe el nombre de la serie que deseas buscar");
+        var nombreSerie = teclado.nextLine();
+        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Arrow&apikey=716b5b12");
+    }
+}
