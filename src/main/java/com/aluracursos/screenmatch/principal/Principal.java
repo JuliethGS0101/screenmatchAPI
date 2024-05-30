20cf81e862c95dc8575c3544a13fb2798685a81f
julig <juliethgs0101@gmail.com>
2024-05-30 18:56:11 -0500

    minimizando codigo

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -1,17 +1,48 @@
 package com.aluracursos.screenmatch.principal;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner teclado = new Scanner(System.in);
-    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String URL_BASE = "https://www.omdbapi.com/?t=";
+    private  final String API_KEY = "&apikey=716b5b12";
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
 
     public void muestraElMenu(){
-        System.out.println("Por favor escribe el nombre de la serie que deseas buscar");
+        System.out.println("Escribe el nombre de la serie que deseas buscar");
+        //Busca los datos generales de las series
         var nombreSerie = teclado.nextLine();
-        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Arrow&apikey=716b5b12");
+        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+
+        //Busca todos los datos de las temporadas
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+            json= consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporadas);
+        }
+
+        //temporadas.forEach(System.out::println);
+
+        //Mostrar solo el titulo de los episodios para las temporadas
+        /*for (int i = 0; i < datos.totalDeTemporadas(); i++) {
+            List<DatosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }*/
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
